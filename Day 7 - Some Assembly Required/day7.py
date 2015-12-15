45ec372f85a34819b977f582efb493a03053a968
Michael Winn <mwinn53@gmail.com>
2015-12-15 06:56:10 -0500

    Still working on day7

--- Day 7 - Some Assembly Required/day7.py
+++ Day 7 - Some Assembly Required/day7.py
@@ -43,7 +43,7 @@
 # After it is run, these are the signals on the wires:
 # 
 #   d: 72
-#    e: 507
+#   e: 507
 #   f: 492
 #   g: 114
 #   h: 65412
@@ -53,3 +53,136 @@
 # 
 # In little Bobby's kit's instructions booklet (provided as your puzzle input),
 # what signal is ultimately provided to wire a?
+
+import re
+
+#############################################################################
+def parseFile(fname):
+
+    cmdList = []
+    
+    f = open(fname, 'r')
+    
+    for line in f:
+        # op, operand1, operand2, dest
+        op = None
+        operand1 = None
+        operand2 = None
+        dest = None
+        
+        string = line.split(' -> ')
+
+        dest = string[1].strip('\n')
+
+        string = string[0].split(' ')
+
+        if len(string) == 1:  # STORE Operation (e.g. 123 -> a)
+            op = 'sto'
+            operand1 = typecast(string[0])
+
+        elif len(string) == 2: # NOT Operation (e.g. NOT 123 -> a)
+            op = 'not'
+            operand1 = typecast(string[1])
+
+        elif len(string) == 3: # Binary operation (e.g. 123 AND 456 -> a)
+            if string[1] == 'AND':
+                op = 'and'
+                
+            elif string[1] == 'OR':
+                op = 'or'
+        
+            elif string[1] == 'LSHIFT':
+                op = 'lsh'
+
+            elif string[1] == 'RSHIFT':
+                op = 'rsh'
+
+            operand1 = typecast(string[0])
+            operand2 = typecast(string[2])
+
+        listItem = [op, operand1, operand2, dest]
+
+        cmdList.append(listItem)
+
+    return cmdList
+#############################################################################
+def typecast(value):
+    if not(re.search('[a-z]{1,2}',value)):
+        return int(value)
+    else:
+        return str(value)
+    
+    
+#############################################################################
+def recurList(circuit, result):
+
+    # find result in circuit
+    for row in circuit:
+        if row[3] == result:
+           print 'FOUND: {}\t(Row {}) \t\t{} {} -> {}'.format(row, circuit.index(row), row[0], row[1], row[3])            
+           circuit.remove(row)
+
+        # Recur if operand1 is not a value
+           if isinstance(row[1], str):
+               print '\t RECURRING on OP1: {}...'.format(row[1])
+               op1 = recurList(circuit, row[1])
+           else:
+               print '\t USING {} for OP1...'.format(row[1])
+               op1 = row[1]
+
+        # Recur if operand2 (if required) is not a value
+           if row[2] == None:
+               op2 = None
+
+           else:
+               if (isinstance(row[2], str)):
+                   print '\t RECURRING on OP2: {}...'.format(row[2])
+                   op2 = recurList(circuit, row[2])
+               else:
+                   print '\t USING {} for OP2...'.format(row[2])
+                   op2 = row[2]
+                   
+        # Base case: result can be evaluated
+           result = evaluate([row[0], op1, op2])
+           print '\t EVALUATED {} {} {} -> {}'.format(row[0], op1, op2, result)
+
+           return result
+
+#############################################################################    
+def evaluate(line):
+
+    op = line[0]
+    operand1 = line[1]
+    operand2 = line[2]
+
+    print 'Evaluating {}...'.format(line)
+
+    if op == 'sto':
+        return operand1
+
+    elif op == 'and':
+        return operand1 & operand2
+
+    elif op == 'or':
+        return operand1 | operand2
+
+    elif op == 'not':
+        return ~ operand1
+
+    elif op == 'lsh':
+        return operand1 << operand2
+
+    elif op == 'rsh':
+        return operand1 >> operand2
+
+#############################################################################
+def main():
+
+    file = 'input.txt'
+    circuit = parseFile(file)
+    result = 'a'
+
+    recurList(circuit, result)
+
+if __name__ == '__main__':
+    main()

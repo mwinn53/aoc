5110cde13568f3dcd748c52f3de8e2fea420d49c
mwinn53 <ltwinn@hotmail.com>
2017-12-03 23:14:56 +0900

    Not the prettiest, but a hard-earned first time GO on both parts!

--- 2017/Day 3 - Spiral Memory/day3.py
+++ 2017/Day 3 - Spiral Memory/day3.py
@@ -25,6 +25,30 @@
 #
 # Your puzzle input is 312051.
 
+# --- Part Two ---
+#
+# As a stress test on the system, the programs here clear the grid and then store the value 1 in square 1. Then, in the
+#  same allocation order as shown above, they store the sum of the values in all adjacent squares, including diagonals.
+#
+# So, the first few squares' values are chosen as follows:
+#
+# Square 1 starts with the value 1.
+# Square 2 has only one adjacent filled square (with value 1), so it also stores 1.
+# Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.
+# Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.
+# Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.
+# Once a square is written, its value does not change. Therefore, the first few squares would receive the following
+#  values:
+#
+# 147  142  133  122   59
+# 304    5    4    2   57
+# 330   10    1    1   54
+# 351   11   23   25   26
+# 362  747  806--->   ...
+# What is the first value written that is larger than your puzzle input?
+#
+# Your puzzle input is still 312051.
+
 def findNumber(goal):
     x = 0
     y = 0
@@ -42,7 +66,7 @@ def findNumber(goal):
         for yy in range(0, i):
             x += sign * 1
             n += 1
-            if n == goal:
+            if n > goal:
                 return x, y
 
         sign = sign * -1    # flip the sign and increment the counter
@@ -52,12 +76,61 @@ def findNumber(goal):
 def findDistance(coords):
 #  Manhattan distance formula: |x1 - x2| + |y1 - y2|
         return (abs(coords[0] - 0) + (abs(coords[1] - 0)))
-#############################################################################
 
+#############################################################################
+def buildMatrix(goal):
+    x = 0
+    y = 0
+    i = 1
+    n = 1
+    sign = 1
+    spiral = {(0,0): 1}
+
+    while n < goal:
+        for xx in range(0, i):
+            x += sign * 1
+
+            n = spiral.get((x+1,y), 0) + \
+                spiral.get((x+1,y+1), 0) + \
+                spiral.get((x,y+1), 0) +\
+                spiral.get((x-1,y+1), 0) +\
+                spiral.get((x-1,y), 0) + \
+                spiral.get((x-1,y-1), 0) + \
+                spiral.get((x,y-1), 0) + \
+                spiral.get((x+1,y-1), 0)
+
+            spiral[(x, y)] = n
+
+            if n > goal:
+                return n
+
+        for yy in range(0, i):
+            y += sign * 1
+
+            n = spiral.get((x + 1, y), 0) + \
+                spiral.get((x + 1, y + 1), 0) + \
+                spiral.get((x, y + 1), 0) + \
+                spiral.get((x - 1, y + 1), 0) + \
+                spiral.get((x - 1, y), 0) + \
+                spiral.get((x - 1, y - 1), 0) + \
+                spiral.get((x, y - 1), 0) + \
+                spiral.get((x + 1, y - 1), 0)
+
+            spiral[(x, y)] = n
+
+            if n > goal:
+                return n
+
+        sign = sign * -1    # flip the sign and increment the counter
+        i += 1
+
+#############################################################################
 
 def main():
     n = 312051
-    print("Result: {}".format(findDistance(findNumber(n))))
+
+    print("Stage 1 Result: {}".format(findDistance(findNumber(n))))
+    print("Stage 2 Result: {}".format(buildMatrix(n)))
 
 if __name__ == '__main__':
     main()
